a41d13d76949de0c20ee23ed1bccf08ff02f5676
Mohammed Naguib <m.naguib2611.com>
2020-01-11 08:45:03 +0200

    Server UI

---
+++ XO-Server/src/serverui/AdminDashboard.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author migo2
+ */
+public class AdminDashboard extends Application {
+    
+    @Override
+    public void start(Stage stageDashboard) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("AdminDashboard.fxml"));
+        
+        Scene sceneDashboard = new Scene(root,800,500);
+       
+        
+        stageDashboard.setScene(sceneDashboard);
+        stageDashboard.setResizable(false);
+        stageDashboard.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ XO-Server/src/serverui/AdminDashboardController.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.concurrent.DelayQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import server.PlayerHandler;
+import server.Server;
+
+/**
+ *
+ * @author migo2
+ */
+public class AdminDashboardController implements Initializable {
+
+    Server server;
+//    String[] onlineTest = {"hello", "hi", "world"};
+    Thread playersThread;
+
+    @FXML
+    private Label serverMessage;
+    @FXML
+    private Button TurnOffServer;
+    @FXML
+    private Button TurnOnServer;
+
+    @FXML
+    private FlowPane flowpaneLeft;
+    @FXML
+    private FlowPane flowpaneRight;
+
+    Thread serverThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            server = new Server();
+            server.startServer();
+        }
+    });
+
+    @FXML
+    private void startConnection(ActionEvent event) {
+        
+        TurnOffServer.setDisable(false);
+        serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                server = new Server();
+                server.startServer();
+            }
+        });
+        serverThread.setDaemon(true);
+        serverThread.start();
+        updateOnline();
+        serverMessage.setText("Server is Running");
+        serverMessage.setStyle("-fx-background-color: lightgreen;");
+        
+        TurnOnServer.setDisable(true);
+    }
+
+    @FXML
+    private void stopConnection(ActionEvent event) {
+        TurnOffServer.setDisable(true);
+        try {
+            server.closeServer();
+        } catch (IOException ex) {
+            Logger.getLogger(AdminDashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        flowpaneLeft.getChildren().clear();
+        flowpaneRight.getChildren().clear();
+        serverThread.stop();
+        playersThread.stop();
+        serverMessage.setText("  Server is Stopped");
+        serverMessage.setStyle("-fx-background-color: lightcoral;");
+        TurnOnServer.setDisable(false);
+
+    }
+
+    @FXML
+    private void onlinePlayers(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+
+    @FXML
+    private void highScores(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        TurnOnServer.setDisable(true);
+        serverMessage.setText("  Server is Running");
+        serverThread.setDaemon(true);
+        serverThread.start();
+        flowpaneLeft.setOrientation(Orientation.VERTICAL);
+        flowpaneRight.setOrientation(Orientation.VERTICAL);
+        updateOnline();
+
+    }
+
+    public void updateOnline() {
+        playersThread =null;
+        
+        playersThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    Platform.runLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            flowpaneLeft.getChildren().clear();
+                            flowpaneRight.getChildren().clear();
+                            for (String onlineTest1 : server.onlineTest) {
+                                Label onlineTst = new Label(onlineTest1);
+                                onlineTst.prefWidth(150);
+                                onlineTst.setTextFill(Color.web("#D4AF37"));
+                                onlineTst.setFont(Font.font("times-new-roman", FontWeight.EXTRA_BOLD, 30));
+
+                                Label onlineTst1 = new Label(onlineTest1);
+                                onlineTst1.prefWidth(150);
+                                onlineTst1.setTextFill(Color.web("#FFDF00"));
+                                onlineTst1.setFont(Font.font("times-new-roman", FontWeight.EXTRA_BOLD, 30));
+                                flowpaneLeft.getChildren().add(onlineTst);
+                                flowpaneRight.getChildren().add(onlineTst1);
+
+                            }
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+//                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        playersThread.setDaemon(true);
+        playersThread.start();
+    }
+}
