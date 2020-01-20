c09258d7dc8a36eea11f758bf9e34c16ba8f5bef
AhmedAdel <ahmedadelfahim@gmail.com>
2020-01-20 16:38:59 +0200

    signup bug fixed

--- XO-Client/src/ui/SignUpView.java
+++ XO-Client/src/ui/SignUpView.java
@@ -60,6 +60,8 @@ public class SignUpView implements Initializable {
     @FXML
     private Label TicTacToe;
 
+    private ChangeListener signUpListener;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Font.loadFont(getClass().getResource("../res/font/Bangers.ttf").toExternalForm(),28);
@@ -68,10 +70,10 @@ public class SignUpView implements Initializable {
 
     private void addListener(){
 
-        ChangeListener<Boolean> x = new ChangeListener<Boolean>() {
+        signUpListener = new ChangeListener<Number>() {
             @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (newValue && !oldValue) {
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (newValue.intValue() == 1) {
 
                     try {
                         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PlayMode.fxml"));
@@ -79,30 +81,27 @@ public class SignUpView implements Initializable {
                         Parent root = fxmlLoader.load();
                         Scene scene = new Scene(root);
                         Stage stage = (Stage) registerButton.getScene().getWindow();
-
                         stage.setScene(scene);
                         stage.setTitle("Tic Tac Toe");
-                        // LogInViewModel.toPlayScreenFlagProperty().removeListener();
 
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                } else if(!newValue && !oldValue){
+                } else if(newValue.intValue() == 0){
 
 
                     System.out.println("FFF");
                 }
-                SignUpViewModel.setToPlayScreenFlag(false);
+                SignUpViewModel.setToPlayScreenFlag(-1);
                 SignUpViewModel.toPlayScreenFlagProperty().removeListener(this);
             }
         };
-        SignUpViewModel.toPlayScreenFlagProperty().addListener(x);
-/*
-        LogInViewModel.toPlayScreenFlagProperty().addListener((observable, oldValue, newValue) -> {
-//            System.out.println(newValue);
+        SignUpViewModel.toPlayScreenFlagProperty().addListener(signUpListener);
 
+    }
 
-        });*/
+    private void removeListeners() {
+        SignUpViewModel.toPlayScreenFlagProperty().removeListener(signUpListener);
     }
 
     @FXML
--- XO-Client/src/viewmodel/SignUpViewModel.java
+++ XO-Client/src/viewmodel/SignUpViewModel.java
@@ -2,29 +2,38 @@ package viewmodel;
 
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import model.ClientSideHandler;
 import utils.Utils;
 
 import java.util.HashMap;
 
 public class SignUpViewModel {
-    private static final BooleanProperty toPlayScreenFlag = new SimpleBooleanProperty();
+    private static final IntegerProperty toPlayScreenFlag = new SimpleIntegerProperty();
     public static void signUp(HashMap<String,Object> map){
 
+        if(ClientSideHandler.getInstance().signUp(Utils.toString(map)))
+        {
+            toPlayScreenFlag.setValue(1);               // 1 > succeeded
+        }
+        else {
+            toPlayScreenFlag.setValue(0);               // 0 > failed
+        }
 
-        toPlayScreenFlag.setValue(ClientSideHandler.getInstance().signUp(Utils.toString(map)));
+
     }
 
-    public static boolean isToPlayScreenFlag() {
+    public static int isToPlayScreenFlag() {
         return toPlayScreenFlag.get();
     }
 
-    public static BooleanProperty toPlayScreenFlagProperty() {
+    public static IntegerProperty toPlayScreenFlagProperty() {
         return toPlayScreenFlag;
     }
 
-    public static void setToPlayScreenFlag(boolean toPlayScreenFlag) {
+    public static void setToPlayScreenFlag(int toPlayScreenFlag) {
         SignUpViewModel.toPlayScreenFlag.set(toPlayScreenFlag);
 
     }
